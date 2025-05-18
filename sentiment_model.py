49cdb1d2ffaa2ea319f1153db7746eeeb9949e46
fatibreezy <69746431+fatibreezy@users.noreply.github.com>
2025-05-18 06:59:30 -0700

    Create sentiment_model.py

---
+++ sentiment_model.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+
+def classify_sentiment(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    if polarity > 0.2:
+        return "Positive"
+    elif polarity < -0.2:
+        return "Negative"
+    else:
+        return "Neutral"
